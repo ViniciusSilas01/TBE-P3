704886977014c99bc7445180ac1dec2e2e68d9cb
ViniciusSilas01 <vinicius2012silas@gmail.com>
2024-09-12 13:59:27 -0300

    Atualizações dentro do código nas respectivas abas controller e service

--- aluno_online/api/src/main/java/br/com/alunoonline/api/controller/AlunoController.java
+++ aluno_online/api/src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
@@ -18,4 +21,23 @@ public class AlunoController {
     public void criarAluno(@RequestBody Aluno aluno) {
         alunoService.criarAluno(aluno);
     }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Aluno> ListarTodosAlunos() {
+        return alunoService.ListarTodosAlunos();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Aluno> buscarAlunoPorId(@PathVariable Long id) {
+        return alunoService.buscarAlunoPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarAlunoPorId(@PathVariable Long id) {
+        alunoService.deletarAlunoPorId(id);
+    }
+
 }
